5740efb032810567b428b055d7215fbab0af257d
Mariam Mawed <mariam.rashad@yahoo.com>
2021-05-22 19:30:03 -0400

    add more hello feature

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("GitHub Repo already published" );
+        System.out.println("Doing more work");
     }
 }
